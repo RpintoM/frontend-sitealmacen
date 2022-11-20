75d208fa5dc3848ee0e8ad0094a83726e279f9d1
Rafael Pinto <100289672+RpintoM@users.noreply.github.com>
2022-11-20 18:09:15 -0500

    Add files via upload

---
+++ src/app/models/cliente.ts
@@ -0,0 +1,8 @@
+export interface ClienteI {
+    id?: number;
+    telefonoCliente: string
+     nombreCliente: string
+    direccionCliente: string
+   correoCliente: string
+    passwordCliente: string
+}
